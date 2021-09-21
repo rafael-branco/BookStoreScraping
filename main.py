cab023d5fc390c7a9466231f389882f0bc1b89a7
Rafael Branco <rafael.rbranco@hotmail.com>
2021-09-20 23:29:04 -0300

    Requesting book page

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ main_url = "https://books.toscrape.com/"
 
 
 for i in range(1, 2):
-    re = requests.get(main_url + "catalogue/page-" + str(i) + ".html")
-    soup = BeautifulSoup(re.text, "html.parser")
+    main_re = requests.get(main_url + "catalogue/page-" + str(i) + ".html")
+    soup = BeautifulSoup(main_re.text, "html.parser")
     books = soup.select("section ol li div.image_container a")
     for item in books:
-        print(item['href'])
+        re_book = requests.get(main_url + item['href'])
+        soup = BeautifulSoup(re_book.text, "html.parser")
+
+    main_re.close()
